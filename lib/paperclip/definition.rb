bf14b11b3a682864816e5293b7ebc601eff926d0
Jon Yurek <jyurek@thoughtbot.com>
2009-02-28 00:06:33 -0500

    More functionality in Definitions. Added Definition::Style.

--- lib/paperclip/definition.rb
+++ lib/paperclip/definition.rb
@@ -1,12 +1,66 @@
 module Paperclip
   class Definition
-    attr_accessor :url, :path, :default_url, :default_path
+    attr_accessor :url, :path, :default_style, :default_path, :whiny_processing
 
     def initialize options = {}
       @url = options[:url]
       @path = options[:path]
-      @default_url = options[:default_url]
+      @default_style = options[:default_style]
       @default_path = options[:default_path]
+      @whiny_processing = options[:whiny_processing]
+      @styles = options[:styles]
+    end
+
+    def style name
+      Style.new
+    end
+
+    class Style
+      attr_accessor :processors, :convert_options, :whiny_processing, :format, :geometry
+      def initialize options = {}, format = nil
+        if options.is_a? String
+          geometry = options
+          options = {}
+        end
+        @geometry         = options.delete(:geomtry) || geometry
+        @format           = options.delete(:format) || format
+        @processors       = options.delete(:processors) || [:thumbnail]
+        @convert_options  = options.delete(:convert_options)
+        @whiny_processing = options.delete(:whiny_processing)
+        @whiny_processing = @whiny_processing.nil? ? true : @whiny_processing
+
+        @methods = []
+        methodize(options)
+      end
+
+      def option_methods
+        @methods
+      end
+
+      def methodize options = {}
+        @methods.concat(options.keys)
+        (class << self; self; end).class_eval do
+          attr_accessor *options.keys
+        end
+        options.each do |key, value|
+          self.send("#{key}=", value)
+        end
+      end
+
+      def merge other
+        options = {
+          :geometry => @geometry,
+          :format => @format,
+          :processors => @processors,
+          :convert_options => @convert_options,
+          :whiny_processing => @whiny_processing
+        }
+        other.option_methods.each do |method|
+          options[method] = other.send(method)
+        end
+
+        Style.new(options)
+      end
     end
   end
 end
--- test/definition_test.rb
+++ test/definition_test.rb
@@ -2,24 +2,109 @@ require 'test/helper'
 
 class DefinitionTest < Test::Unit::TestCase
   context "A definition" do
-    should "accept a :url a parameter and be able to return it" do
-      d = Paperclip::Definition.new(:url => "my_url")
-      assert_equal "my_url", d.url
+    [:url, :path, :default_path, :default_style, :whiny_processing].each do |field|
+      should "accept a #{field.inspect} a parameter and be able to return it" do
+        d = Paperclip::Definition.new(field => "my_#{field}")
+        assert_equal "my_#{field}", d.send(field)
+      end
     end
 
-    should "accept a :path a parameter and be able to return it" do
-      d = Paperclip::Definition.new(:path => "my_path")
-      assert_equal "my_path", d.path
+    should "accept a :styles hash and make its keys available via #style and return Style objects" do
+      d = Paperclip::Definition.new(:styles => {:thumb => {}})
+      assert_kind_of Paperclip::Definition::Style, d.style(:thumb)
     end
 
-    should "accept a :default_path a parameter and be able to return it" do
-      d = Paperclip::Definition.new(:default_path => "my_default_path")
-      assert_equal "my_default_path", d.default_path
+    should "normalize hashes passed as styles to a standard format" do
+      d = Paperclip::Definition.new(:styles => {:thumb => {}})
+      assert_equal [:thumbnail], d.style(:thumb).processors
     end
+  end
 
-    should "accept a :default_url a parameter and be able to return it" do
-      d = Paperclip::Definition.new(:default_url => "my_default_url")
-      assert_equal "my_default_url", d.default_url
+  context "A style accepting an non-empty hash" do
+    setup do
+      @style = Paperclip::Definition::Style.new({
+        :key => "value",
+        :convert_options => "123",
+        :processors => [:none],
+        :whiny_processing => false
+      })
+    end
+    should "access the :key key with a accessor" do
+      assert_equal "value", @style.key
+    end
+    should "not set the default processors" do
+      assert_equal [:none], @style.processors
+    end
+    should "not set the default convert_options" do
+      assert_equal "123", @style.convert_options
+    end
+    should "not set the default whiny value" do
+      assert_equal false, @style.whiny_processing
+    end
+  end
+
+  context "A style accepting an empty hash" do
+    setup{ @style = Paperclip::Definition::Style.new({}) }
+    should "set the default processors" do
+      assert_equal [:thumbnail], @style.processors
+    end
+    should "set the default convert_options" do
+      assert_equal nil, @style.convert_options
+    end
+    should "set the default whiny value" do
+      assert_equal true, @style.whiny_processing
+    end
+  end
+
+  context "A style accepting a geometry string and format" do
+    setup{ @style = Paperclip::Definition::Style.new("100x100", :png) }
+    should "set the format to png" do
+      assert_equal :png, @style.format
+    end
+    should "set the geometry to the argument" do
+      assert_equal "100x100", @style.geometry
+    end
+    should "set the default processors" do
+      assert_equal [:thumbnail], @style.processors
+    end
+    should "set the default convert_options" do
+      assert_equal nil, @style.convert_options
+    end
+    should "set the default whiny value" do
+      assert_equal true, @style.whiny_processing
+    end
+  end
+
+  context "A style accepting a geometry string" do
+    setup{ @style = Paperclip::Definition::Style.new("100x100") }
+    should "set the format to nil" do
+      assert_nil @style.format
+    end
+    should "set the geometry to the argument" do
+      assert_equal "100x100", @style.geometry
+    end
+    should "set the default processors" do
+      assert_equal [:thumbnail], @style.processors
+    end
+    should "set the default convert_options" do
+      assert_equal nil, @style.convert_options
+    end
+    should "set the default whiny value" do
+      assert_equal true, @style.whiny_processing
+    end
+  end
+
+  context "A style object" do
+    setup{ @style = Paperclip::Definition::Style.new({}) }
+    should "create accessors from keys when sent to #methodize" do
+      @style.methodize(:one => "two")
+      assert @style.respond_to?(:one)
+      assert_equal "two", @style.one
+    end
+    should "merge another style when sent #merge!" do
+      @style2 = Paperclip::Definition::Style.new({:one => "two"})
+      @style3 = @style.merge(@style2)
+      assert_equal "two", @style3.one
     end
   end
 end
