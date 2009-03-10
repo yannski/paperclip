b601f9f273939031e46ab0be7ff4df739d0df0c6
Jon Yurek <jyurek@thoughtbot.com>
2009-03-09 22:52:35 -0400

    Refactored Style and added Storage. Added an Options superclass for flexibility.

--- lib/paperclip/definition.rb
+++ lib/paperclip/definition.rb
@@ -1,6 +1,7 @@
 module Paperclip
   class Definition
-    attr_accessor :url, :path, :default_style, :default_path, :whiny_processing
+    attr_accessor :url, :path, :default_style, :default_path, :whiny_processing,
+                  :storage, :storage_method
 
     def initialize options = {}
       @url = options[:url]
@@ -9,57 +10,105 @@ module Paperclip
       @default_path = options[:default_path]
       @whiny_processing = options[:whiny_processing]
       @styles = options[:styles]
+
+      @storage_method = options[:storage] || :filesystem
+      @storage = Storage.for(@storage_method, options)
     end
 
     def style name
       Style.new
     end
 
-    class Style
-      attr_accessor :processors, :convert_options, :whiny_processing, :format, :geometry
-      def initialize options = {}, format = nil
-        if options.is_a? String
-          geometry = options
-          options = {}
+    class Options
+      def initialize options = {}
+        @methods ||= []
+        options.each do |key, value|
+          methodize(key, value)
         end
-        @geometry         = options.delete(:geomtry) || geometry
-        @format           = options.delete(:format) || format
-        @processors       = options.delete(:processors) || [:thumbnail]
-        @convert_options  = options.delete(:convert_options)
-        @whiny_processing = options.delete(:whiny_processing)
-        @whiny_processing = @whiny_processing.nil? ? true : @whiny_processing
-
-        @methods = []
-        methodize(options)
       end
 
       def option_methods
         @methods
       end
 
-      def methodize options = {}
-        @methods.concat(options.keys)
+      def methodize method, value
+        (@methods ||= []) << method.to_sym
         (class << self; self; end).class_eval do
-          attr_accessor *options.keys
+          attr_accessor method
         end
-        options.each do |key, value|
-          self.send("#{key}=", value)
+        instance_variable_set("@#{method}", value)
+      end
+
+      def method_missing method, *args, &block
+        method, assign = method.to_s.split(/(=)/)
+        if assign
+          methodize(method, args.first)
+        elsif block
+          methodize(method, block)
+        else
+          nil
         end
       end
 
       def merge other
-        options = {
-          :geometry => @geometry,
-          :format => @format,
-          :processors => @processors,
-          :convert_options => @convert_options,
-          :whiny_processing => @whiny_processing
-        }
+        result = self.class.new
+        option_methods.each do |method|
+          result.send("#{method}=", self.send(method))
+        end
         other.option_methods.each do |method|
-          options[method] = other.send(method)
+          result.send("#{method}=", other.send(method))
+        end
+        result
+      end
+    end
+
+    class Storage < Options
+      def self.for(method, options = {})
+        self.const_get(method.to_s.camelize).new(options)
+      end
+
+      class Filesystem < Storage
+        def initialize options = {}
+          self.method = :filesystem
+          super(options)
+        end
+      end
+
+      class S3 < Storage
+        attr_reader :permissions
+        def initialize options = {}
+          self.method      = :s3
+          self.bucket      = options.delete(:bucket)
+          self.permissions = options.delete(:s3_permissions)
+          self.headers     = options.delete(:s3_headers)
+          self.host_alias  = options.delete(:s3_host_alias)
+          self.protocol    = options.delete(:s3_protocol)
+          super(options)
         end
 
-        Style.new(options)
+        def permissions= yaml
+          case yaml
+          when Hash then @permissions = yaml
+          when IO, StringIO then @permissions = YAML.load(yaml)
+          when String then @permissions = YAML.load_file(yaml)
+          end
+        end
+      end
+    end
+
+    class Style < Options
+      def initialize options = {}, format = nil
+        if options.is_a? String
+          geometry = options
+          options = {}
+        end
+        self.geometry         = options.delete(:geomtry) || geometry
+        self.format           = options.delete(:format) || format
+        self.processors       = options.delete(:processors) || [:thumbnail]
+        self.convert_options  = options.delete(:convert_options)
+        self.whiny_processing = options.delete(:whiny_processing)
+        self.whiny_processing = whiny_processing.nil? ? true : whiny_processing
+        super(options)
       end
     end
   end
--- test/definition_test.rb
+++ test/definition_test.rb
@@ -8,15 +8,93 @@ class DefinitionTest < Test::Unit::TestCase
         assert_equal "my_#{field}", d.send(field)
       end
     end
-
     should "accept a :styles hash and make its keys available via #style and return Style objects" do
       d = Paperclip::Definition.new(:styles => {:thumb => {}})
       assert_kind_of Paperclip::Definition::Style, d.style(:thumb)
     end
-
     should "normalize hashes passed as styles to a standard format" do
       d = Paperclip::Definition.new(:styles => {:thumb => {}})
       assert_equal [:thumbnail], d.style(:thumb).processors
+    end
+    context "returning a Storage object on call to #storage" do
+      setup{ @definition = Paperclip::Definition.new }
+      should "return a Storage object" do
+        assert_kind_of Paperclip::Definition::Storage, @definition.storage
+      end
+      should "return the default method" do
+        assert_equal :filesystem, @definition.storage.method
+      end
+    end
+    context "returning a Storage object configured for S3" do
+      setup{ @definition = Paperclip::Definition.new :storage => :s3,
+                                                     :s3_permissions => StringIO.new("key: value"),
+                                                     :s3_options => {:one => :two},
+                                                     :s3_protocol => "https",
+                                                     :s3_headers => {'Content-type' => 'text/plain'},
+                                                     :s3_host_alias => "test.example.com",
+                                                     :bucket => "bucket" }
+      should "return a Storage object with an S3 method" do
+        assert_equal :s3, @definition.storage.method
+      end
+      should "return the bucket when asked" do
+        assert_equal "bucket", @definition.storage.bucket
+      end
+      should "return the headers when asked" do
+        assert_equal({'Content-type' => 'text/plain'}, @definition.storage.headers)
+      end
+      should "return the host_alias when asked" do
+        assert_equal "test.example.com", @definition.storage.host_alias
+      end
+      should "return the protocol when asked" do
+        assert_equal "https", @definition.storage.protocol
+      end
+      should "return the permissions when asked" do
+        assert_equal({'key' => 'value'}, @definition.storage.permissions)
+      end
+    end
+  end
+
+  context "An Options object" do
+    setup{ @options = Paperclip::Definition::Options.new }
+    should "create an accessor when unknown setters are called" do
+      assert ! @options.respond_to?(:non_method)
+      @options.non_method = "123"
+      assert_equal "123", @options.non_method
+    end
+    should "create an accessor when blocks are passed to unknown methods" do
+      block = lambda{ puts "Hi!" }
+      assert ! @options.respond_to?(:non_method)
+      @options.non_method &block
+      assert_equal block, @options.non_method
+    end
+    should "return nil when unknown getters are called" do
+      assert ! @options.respond_to?(:non_method)
+      assert_nil @options.non_method
+    end
+    should "merge with other options instances" do
+      options2 = Paperclip::Definition::Options.new
+      options2.key = "value"
+      @options.something = "something"
+      result = @options.merge(options2)
+      assert_equal "value", result.key
+      assert_equal "something", result.something
+    end
+  end
+
+  context "An Options object being initialized" do
+    setup{ @options = Paperclip::Definition::Options.new(:one => :two, :three => :four) }
+    should "have methods named after the keys in the argument hash" do
+      assert_equal :two, @options.one
+      assert_equal :four, @options.three
+    end
+  end
+
+  context "A Storage" do
+    should "return an S3 subclass when asked with #for" do
+      assert Paperclip::Definition::Storage.for(:s3).is_a?(Paperclip::Definition::Storage::S3)
+    end
+    should "return a Filesystem subclass when asked with #for" do
+      assert Paperclip::Definition::Storage.for(:filesystem).is_a?(Paperclip::Definition::Storage::Filesystem)
     end
   end
 
@@ -94,17 +172,22 @@ class DefinitionTest < Test::Unit::TestCase
     end
   end
 
-  context "A style object" do
-    setup{ @style = Paperclip::Definition::Style.new({}) }
-    should "create accessors from keys when sent to #methodize" do
-      @style.methodize(:one => "two")
-      assert @style.respond_to?(:one)
-      assert_equal "two", @style.one
+  context "A style accepting a hash" do
+    setup{ @style = Paperclip::Definition::Style.new(:geometry => "100x100", :format => :png) }
+    should "set the format to the argument" do
+      assert_equal :png, @style.format
     end
-    should "merge another style when sent #merge!" do
-      @style2 = Paperclip::Definition::Style.new({:one => "two"})
-      @style3 = @style.merge(@style2)
-      assert_equal "two", @style3.one
+    should "set the geometry to the argument" do
+      assert_equal "100x100", @style.geometry
+    end
+    should "set the default processors" do
+      assert_equal [:thumbnail], @style.processors
+    end
+    should "set the default convert_options" do
+      assert_equal nil, @style.convert_options
+    end
+    should "set the default whiny value" do
+      assert_equal true, @style.whiny_processing
     end
   end
 end
