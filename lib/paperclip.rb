723edae27b6b84b5c8f63d2549526918c02c3997
Jon Yurek <jyurek@thoughtbot.com>
2009-10-23 09:36:02 -0400

    Added Paperclip.run for shelling out

--- lib/paperclip.rb
+++ lib/paperclip.rb
@@ -15,6 +15,43 @@ require "paperclip/validations"
 module Paperclip
   VERSION = "3.0.0"
 
+  class CommandLineError < PaperclipError
+    attr_accessor :result_code
+    def initialize(message, result_code)
+      super(message)
+      @result_code = result_code
+    end
+  end
+  class CommandNotFound < PaperclipError; end
+
+  def self.run(command, arguments = nil, *expected_values)
+    expected_values = [0] if expected_values.empty?
+    full_command = [options[:command_path], command].compact.join(File::Separator)
+    full_command << " #{arguments}"
+    full_command << " 2>#{bit_bucket}" if options[:swallow_stderr]
+
+    output = `#{full_command}`
+    raise CommandNotFound.new(full_command)              if run_result == 127
+    raise CommandLineError.new(full_command, run_result) unless expected_values.include?(run_result)
+
+    output
+  end
+
+  def self.bit_bucket
+    File.exists?("/dev/null") ? "/dev/null" : "NUL"
+  end
+
+  def self.run_result
+    $?.exitstatus
+  end
+
+  def self.options
+    @options ||= {
+      :command_path => nil,
+      :swallow_stderr => false
+    }
+  end
+
   def self.included(base)
     File.send(:include, UploadedFile)
     StringIO.send(:include, UploadedFile)
--- test/unit/paperclip_test.rb
+++ test/unit/paperclip_test.rb
@@ -7,10 +7,81 @@ class PaperclipTest < Test::Unit::TestCase
   end
 
   should "include UploadedFile into File" do
+    File.stubs(:include)
+    silence_warnings do
+      EmptyClass = Class.new
+      EmptyClass.class_eval do
+        include Paperclip
+      end
+    end
+
+    assert_received(File, :include){|f| f.with(Paperclip::UploadedFile) }
   end
 
   should "include UploadedFile into StringIO" do
+    StringIO.stubs(:include)
+    silence_warnings do
+      EmptyClass = Class.new
+      EmptyClass.class_eval do
+        include Paperclip
+      end
+    end
+
+    assert_received(StringIO, :include){|f| f.with(Paperclip::UploadedFile) }
   end
 
+  should "shell the command out with Paperclip.run" do
+    Paperclip.options[:command_path] = nil
+    return_value = Paperclip.run("echo", "Hello")
+    assert_equal "Hello\n", return_value
+  end
+
+  context "Paperclip.run" do
+    setup do
+      Paperclip.stubs(:`)
+      Paperclip.options[:command_path] = nil
+      Paperclip.options[:swallow_stderr] = false
+    end
+
+    should "prepend Paperclip.options[:command_path] to commands if it is set" do
+      Paperclip.options[:command_path] = "/bin"
+      Paperclip.run("echo", "Hello")
+      assert_received(Paperclip, :`){|s| s.with("/bin/echo Hello") }
+    end
+
+    should "not prepend Paperclip.options[:command_path] to commands if it is nil" do
+      Paperclip.run("echo", "Hello")
+      assert_received(Paperclip, :`){|s| s.with("echo Hello") }
+    end
+
+    should "raise Paperclip::CommandLineError if the result value is in the expected values list" do
+      Paperclip.stubs(:run_result).returns(1)
+      assert_nothing_raised{ Paperclip.run("echo", "Hello", 1) }
+    end
+
+    should "raise Paperclip::CommandLineError if the result value is not in the expected_values list" do
+      Paperclip.stubs(:run_result).returns(1)
+      assert_raises(Paperclip::CommandLineError){ Paperclip.run("echo", "Hello", 2) }
+    end
+
+    should "raise Paperclip::CommandNotFound if #run returns an exit code of 127" do
+      Paperclip.stubs(:run_result).returns(127)
+      assert_raises(Paperclip::CommandNotFound){ Paperclip.run("echo", "Hello") }
+    end
+
+    should "redirect stderr to /dev/null on unix systems when :swallow_stderr is true" do
+      Paperclip.options[:swallow_stderr] = true
+      File.stubs(:exists?).with("/dev/null").returns(true)
+      Paperclip.run("echo", "Hello")
+      assert_received(Paperclip, :`){|s| s.with("echo Hello 2>/dev/null") }
+    end
+
+    should "redirect stderr to NUL on non-unix systems when :swallow_stderr is true" do
+      Paperclip.options[:swallow_stderr] = true
+      File.stubs(:exists?).with("/dev/null").returns(false)
+      Paperclip.run("echo", "Hello")
+      assert_received(Paperclip, :`){|s| s.with("echo Hello 2>NUL") }
+    end
+  end
 end
 
