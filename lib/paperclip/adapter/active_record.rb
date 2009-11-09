0cf6aa57162c04bcbe3e478fcf94766fd16c9ef8
Jon Yurek <jyurek@thoughtbot.com>
2009-11-09 08:07:23 -0500

    Added more callbacks to AR adapter

--- lib/paperclip/adapter/active_record.rb
+++ lib/paperclip/adapter/active_record.rb
@@ -6,10 +6,18 @@ module Paperclip
         base.extend(self)
       end
 
-      def install_attachment_callbacks
+      def install_attachment_callbacks(attachment, styles)
         after_save     :flush_attachments
         before_destroy :clear_attachment
         after_destroy  :flush_attachments
+
+        define_callback :"before_#{attachment}_process"
+        define_callback :"after_#{attachment}_process"
+
+        styles.each do |style|
+          define_callback :"before_#{attachment}_#{style}_process"
+          define_callback :"after_#{attachment}_#{style}_process"
+        end
       end
 
     end
